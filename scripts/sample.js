beed70b9fdec1e77cfa4253dfd3d9bcf6b6ab24c
Anon <no@thanks.com>
2013-02-15 10:23:49 +0000

    Initial

---
+++ scripts/sample.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+     var wm4 = WorldMap({
+        id: 'map4',
+        selector: '#svgWorldMap4',
+        scale: 0.2,
+        margin: '0',
+        top: '50',
+        height: '300',
+        width: '550',
+        inactiveCountryFill: '#686',
+        inactiveCountryStroke: '#fff',
+        inactiveCountryStrokeWidth: 6,
+        showCountryBoxOnMouserEnter: false,
+        drawNorthAmerica: true,
+        drawCentralAmerica: true,
+        drawSouthAmerica: true,
+        drawEurope: true,
+        drawAfrica: true,
+        drawAsia: true,
+        drawOceania: true,
+        drawAntarctic: true,
+        onCountryMouseEnter: function (config) {
+            var id = config.countryId;
+        },
+        onCountryMouseMove: function (config) {
+            var id = config.countryId;
+        },
+        onCountryMouseOut: function (config) {
+            var id = config.countryId;
+        },
+        onCountryMouseClick: function (countryId) {
+            var id = countryId;
+        }
+    });
+});
