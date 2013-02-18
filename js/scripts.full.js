e3429f6f9bb1727e5ff1b6f1fdecbc0b2315c337
Anon <no@thanks.com>
2013-02-18 10:34:02 +0000

    new initial

---
+++ js/scripts.full.js
@@ -0,0 +1,11 @@
+$(document).ready(function()
+{
+	$( document ).bind( "mobileinit", function() {
+      // jQuery Mobile configuration
+      $.mobile.pushStateEnabled = false;
+	  $.mobile.allowCrossDomainPages = true;
+	});
+	
+  // Custom code here!
+  
+});
